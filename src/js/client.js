cb9a6e645b4426d33db01178d5a3f4e71352c4b2
Kira98k <kira98k.d.law@gmail.com>
2020-08-20 18:26:19 +0530

    Testing Card Buttons

--- src/js/client.js
+++ src/js/client.js
@@ -1,9 +1,19 @@
 console.log("Hello World!!!");
 
 TrelloPowerUp.initialize({
-  'card-detail-badges': function(trello, options){
+  'card-detail-badges': function (trello, options) {
     return [{
       text: 'My Badge',
     }];
   },
+  'card-buttons': function (trello, options) {
+    return [{
+      icon: "./icon.png",
+      text: 'Open Popup',
+      callback: function () {
+        alert("test");
+      },
+      condition: 'edit'
+    }]
+  }
 });
