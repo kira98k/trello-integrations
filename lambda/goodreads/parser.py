decfc2d2f2383b63ce2a3d5b661a40cc150ecf5d
Kira98k <kira98k.d.law@gmail.com>
2020-08-28 19:04:47 +0530

    Fixing XML Parsing. Closes #9

--- lambda/goodreads/parser.py
+++ lambda/goodreads/parser.py
@@ -8,15 +8,15 @@ def parse_user_id(content):
 def parse_user_books(content):
 	root = etree.fromstring(content)
 	books = []
-	for item in root.elem('//item'):
+	for item in root.xpath('//item'):
 		book = {
 			"book_id": item.xpath("book_id/text()")[0],
 			"title": item.xpath("title/text()")[0],
 			"author": item.xpath("author_name/text()")[0],
 			"description": item.xpath("book_description/text()")[0],
 			"image_url": item.xpath("book_large_image_url/text()")[0],
-			"shelves": item.xpath("user_shelves/text()")[0].split(", "),
-			"pages": item.xpath("book/num_pages/text()")[0]
+			"shelves": (item.xpath("user_shelves/text()") + [""])[0].split(", "),
+			"pages": (item.xpath("book/num_pages/text()") + [""])[0]
 		}
 		if "to-read" in book["shelves"]:
 			book["status"] = "to-read"
@@ -31,7 +31,7 @@ def parse_user_books(content):
 def parse_search_books(content):
 	root = etree.fromstring(content)
 	books = []
-	for item in root.item("//best_book"):
+	for item in root.xpath("//best_book"):
 		book = {
 			"book_id": item.xpath("id/text()")[0],
 			"title": item.xpath("title/text()")[0],
