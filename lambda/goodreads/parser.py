a56beefab761db9aa78fbcde2b00a9dddfefb371
Kira98k <kira98k.d.law@gmail.com>
2020-08-29 14:03:00 +0530

    Revert "Removing LXML Dependency. Closes #11"
    This reverts commit 18bb9f92

--- lambda/goodreads/parser.py
+++ lambda/goodreads/parser.py
@@ -1,22 +1,22 @@
-from xml.etree import ElementTree as ETree
+from lxml import etree
 
 
 def parse_user_id(content):
-	return ETree.fromstring(content).findall("user")[0].get("id")
+	return etree.fromstring(content).xpath("//user/@id")[0]
 
 
 def parse_user_books(content):
-	root = ETree.fromstring(content)
+	root = etree.fromstring(content)
 	books = []
-	for item in root.findall('channel/item'):
+	for item in root.xpath('//item'):
 		book = {
-			"book_id": item.findall("book_id")[0].text,
-			"title": item.findall("title")[0].text,
-			"author": item.findall("author_name")[0].text,
-			"description": item.findall("book_description")[0].text,
-			"image_url": item.findall("book_large_image_url")[0].text,
-			"shelves": (item.findall("user_shelves")[0].text or "").split(", "),
-			"pages": (item.findall("book/num_pages")[0].text or "")
+			"book_id": item.xpath("book_id/text()")[0],
+			"title": item.xpath("title/text()")[0],
+			"author": item.xpath("author_name/text()")[0],
+			"description": item.xpath("book_description/text()")[0],
+			"image_url": item.xpath("book_large_image_url/text()")[0],
+			"shelves": (item.xpath("user_shelves/text()") + [""])[0].split(", "),
+			"pages": (item.xpath("book/num_pages/text()") + [""])[0]
 		}
 		if "to-read" in book["shelves"]:
 			book["status"] = "to-read"
@@ -29,14 +29,14 @@ def parse_user_books(content):
 
 
 def parse_search_books(content):
-	root = ETree.fromstring(content)
+	root = etree.fromstring(content)
 	books = []
-	for item in root.findall("search/results/work/best_book"):
+	for item in root.xpath("//best_book"):
 		book = {
-			"book_id": item.findall("id")[0].text,
-			"title": item.findall("title")[0].text,
-			"author": item.findall("author/name")[0].text,
-			"image_url": item.findall("image_url")[0].text,
+			"book_id": item.xpath("id/text()")[0],
+			"title": item.xpath("title/text()")[0],
+			"author": item.xpath("author/name/text()")[0],
+			"image_url": item.xpath("image_url/text()")[0],
 		}
 		books.append(book)
 	return books
--- lambda/handler.py
+++ lambda/handler.py
@@ -3,10 +3,10 @@ import json
 from goodreads import goodreads
 
 
-def handler(event, context):
+def main_handler(event, context):
 	action = event.pop("action")
-	goodreads_handler = getattr(goodreads, action)
-	body = goodreads_handler(**event)
+	handler = getattr(goodreads, action)
+	body = handler(**event)
 	return {
 		"statusCode": 200,
 		"body": json.dumps(body)
