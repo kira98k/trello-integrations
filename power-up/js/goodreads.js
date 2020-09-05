4112d864445a6220a3092c6cff611df96ec2855b
Kira98k <kira98k.d.law@gmail.com>
2020-09-05 16:25:13 +0530

    Added Github Pages Module To Deploy. Closes #12

--- power-up/js/goodreads.js
+++ power-up/js/goodreads.js
@@ -3,21 +3,21 @@ import axios from 'axios';
 const BASE_URL = process.env.BASE_URL;
 
 function postData(data) {
-    return axios.post(BASE_URL, data).then(response => response.data);
+	return axios.post(BASE_URL, data).then(response => response.data);
 }
 
 export function create_request() {
-    return postData({"action": "create_request"});
+	return postData({ "action": "create_request" });
 }
 
 export function get_access_token(request_token) {
-    return postData({"action": "get_access_token", "request_token": request_token});
+	return postData({ "action": "get_access_token", "request_token": request_token });
 }
 
 export function get_books(access_token) {
-    return postData({"action": "get_books", "access_token": access_token});
+	return postData({ "action": "get_books", "access_token": access_token });
 }
 
 export function search_books(access_token) {
-    return postData({"action": "search_books", "access_token": access_token});
+	return postData({ "action": "search_books", "access_token": access_token });
 }
